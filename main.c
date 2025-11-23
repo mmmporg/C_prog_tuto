71b46931cff97e88c266829f45202aea38adefed
bushido <tenshiienoko51@gmail.com>
2025-11-23 18:29:13 +0800

    general form of a C program

--- main.c
+++ main.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
 
 int main(){
-	printf("hello world");
+	
+	printf("hello world\n"); //this statement prints "hello world" to the console
+	printf("life: 100\n");
+	printf("score: 2500\n");
+	printf("level: 5");
+	printf("game over");
+	puts("end of program");
 }
