e8dd15d0bd24a52eba7a51f94aca91c38a7e7caf
bushido <tenshiienoko51@gmail.com>
2025-11-25 16:28:00 +0800

    data manipulation next

--- data_manipulation.c
+++ data_manipulation.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#define PI 3.14
 
 int main() {
     /**
@@ -6,6 +7,10 @@ int main() {
      * you can display a data
      * you can store a data
      * you can computes with 2 or more datas
+     * 
+     * you can read data from user input
+     * you can define constant
+     * you cannot use keywords like identifier
      */
 
     // display data
@@ -28,10 +33,24 @@ int main() {
     int number3 = number1 + x;
     printf("%d\n", number3);
 
-    printf("%d\n", sizeof(short));
-    printf("%d\n", sizeof(int));
-    printf("%d\n", sizeof(float));
-    printf("%d\n", sizeof(char));
+    printf("%ld\n", sizeof(short));
+    printf("%ld\n", sizeof(int));
+    printf("%ld\n", sizeof(float));
+    printf("%ld\n", sizeof(char));
 
+    //read data from user
+    int var;
+    printf("Enter a number: ");
+    scanf("%d", &var);
+    printf("The value that I put is: %d\n", var);
+
+    //define a constant
+    const int var2 = 3;
+    printf("The value of the constant is: %d\n", var2);
+    printf("The constant define at the beginning is: %f\n", PI);
+
+    //you cannot do it
+    //int return = 0;
+    
     return 0;
 }
